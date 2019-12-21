1729ee2280fdea1d76f5846500ee7da3114ae1d0
Carlos Brusil <44129243+carlosbrusil@users.noreply.github.com>
2019-12-20 22:37:58 -0500

    capitulos 5 y 6
    para terminar curso de js para principiantes

---
+++ c5_Iterador_Bucles.js
@@ -0,0 +1,11 @@
+let count=1;
+while (count<=10){
+    console.log(count,".-  Tecnosig21");
+    count=count+1;
+}
+//
+console.log("");
+let usuario=['carlos','mayte','carolina','mateo'];
+for (let i=0;i<usuario.length;i++){
+    console.log(i,".- ",usuario[i]);
+}
---
+++ c6_funciones.js
@@ -0,0 +1,9 @@
+function suma(a,b){
+    console.log(a+"+"+b+"="+(a+b));
+
+}
+let num1=45;
+let num2=63;
+console.log("");
+console.log("FUNCION SUMA");
+suma(num1,num2);
